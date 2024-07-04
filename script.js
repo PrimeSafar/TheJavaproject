47a757cdea48cece6b392eb07e218df3b0412cdf
PrimeSafar <152369965+PrimeSafar@users.noreply.github.com>
2024-07-04 12:16:14 +0300

    NewADd

--- script.js
+++ script.js
@@ -23,56 +23,100 @@ function playRound(humanChoice)
    
     let computerChoice = getComputerChoice();
     let resultMessage = '';
+    let resetMassage = ''
             if(humanChoice === "rock" && computerChoice === "rock")
         {
             resultMessage = "It's a tie!";
+            document.getElementById('result').style.color ="black"
+
                 }
         else if(humanChoice === "rock" && computerChoice === "scissors")
             {
             humanScore++
             resultMessage = "You win!";
+            document.getElementById('result').style.color ="green"
             }
         else if(humanChoice === "rock" && computerChoice === "paper")
             {
             computerScore++
             resultMessage = "Computer wins!";
+            document.getElementById('result').style.color ="red"
+
             }
         else if(humanChoice === "paper" && computerChoice === "rock")
             {
             humanScore++
             resultMessage = "You win!";
+            document.getElementById('result').style.color ="green"
+
             }
         else if(humanChoice === "paper" && computerChoice === "scissors")
             {
             computerScore++
             resultMessage = "Computer wins!";
+            document.getElementById('result').style.color ="red"
+
             }
         else if(humanChoice === "paper" && computerChoice === "paper")
             {
                 resultMessage = "It's a tie!";
+                document.getElementById('result').style.color ="black"
+
             }
         else if(humanChoice === "scissors" && computerChoice === "rock")
             {
             computerScore++
             resultMessage = "Computer wins!";
+            document.getElementById('result').style.color ="red"
+
             }
         else if(humanChoice === "scissors" && computerChoice === "paper")
             {
             humanScore++
             resultMessage = "You win!";
+            document.getElementById('result').style.color ="green"
+
             }
         else if(humanChoice === "scissors" && computerChoice === "scissors")
             {
                 resultMessage = "It's a tie!";
+                document.getElementById('result').style.color ="black"
+
             }
         else
         {
             console.log("Invalid choice. Please enter rock, paper, or scissors.")
             
         }
-        document.getElementById('computerChoice').innerText = `Computer Choice = ${computerChoice}`;
-        document.getElementById('HumanChoice').innerText = `Human Choice = ${humanChoice}`;
+        if (humanScore === 5) {
+            document.getElementById('score').innerText = `Human: ${humanScore}`
+            document.getElementById('result').innerText = 'YOU WIN!';
+            return;
+        } else if (computerScore === 5) {
+            document.getElementById('score').innerText = `Computer : ${computerScore}`
+            document.getElementById('result').innerText = 'YOU LOSE!';
+            return;
+        }
+        else if(humanScore&&computerScore === 5)
+        {
+            document.getElementById('result').innerText = 'Its Tie !';
+            return;
 
-document.getElementById('result').innerText ='Result IS =' + resultMessage 
-document.getElementById('score').innerText = `Human: ${humanScore} - Computer: ${computerScore}`;
+        }
+        document.getElementById('score').innerText = `Human: ${humanScore} - Computer: ${computerScore}`;
+        document.getElementById('computerChoice').innerText = `Computer Choice: ${computerChoice}`;
+        document.getElementById('HumanChoice').innerText = `Human Choice: ${humanChoice}`;
+
+document.getElementById('result').innerText ='Result Is: ' + resultMessage 
+document.getElementById('score').innerText = `Human: ${humanScore} 
+ Computer: ${computerScore}`;
+}
+function resetGame()
+{
+    humanScore = 0;
+    computerScore = 0;
+    document.getElementById('result').innerText = "Reset Game !  ";  // Reset the result message to empty string.
+    document.getElementById('score').innerText = "Human: 0 - Computer: 0";
+    document.getElementById('computerChoice').innerText = "Computer Choice: ";
+    document.getElementById('HumanChoice').innerText = "Human Choice: ";
 }
